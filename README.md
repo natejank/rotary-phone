8bba6e4777e59fb1291ce0cead38ecbe9d4e91b4
Nathan Jankowski <45954802+natejank@users.noreply.github.com>
2022-12-27 22:13:33 -0500

    Initialization (#1)
    * init.py bringup
    
    create db with tables
    
    * add database documentation to readme
    
    * create initialization script and database lib
    
    libphone is the start of a library for interacting
    with the phone

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Rotary Payphone Hacking Project
+
+## todo
+
+[] config file for database location
+[] control bitrate and size of sound files
+
+## Table schema
+
+### Phone number entries
+
+Name: `numbers`
+
+| keys         | type                  | related to |
+| :-----       | :-------------------- | :--------- |
+| id           | `INTEGER PRIMARY KEY` |            |
+| number       | `UNIQUE INTEGER`      |            |
+| sound        | `BLOB`                |            |
+| description  | `TEXT`                |            |
---
+++ libphone/database/db.py
@@ -0,0 +1,61 @@
+from os import PathLike
+from sqlite3 import Cursor
+
+"""
+Functions for working with the database
+
+:author: Nathan Jankowski (njj3397 [at] rit [dot] edu)
+"""
+
+
+def table_exists(cursor: Cursor, name: str):
+    """
+    Checks if a table exists
+
+    :param cursor: database cursor
+    :param name: table name
+    :return: true if table exists
+    """
+    cursor.execute(
+        'SELECT name FROM sqlite_schema WHERE type="table" AND name=?',
+        (name,))
+    return len(cursor.fetchall()) != 0
+
+
+def create_entry(cursor: Cursor, number: int, sound: PathLike | str, description='') -> None:
+    """
+    Creates a database entry for a phone number
+
+    :param cursor: database cursor
+    :param number: telephone number
+    :param sound: Path to sound file
+    :param description: Entry description (optional)
+    """
+    # TODO control bitrate and size of sound files
+    cursor.execute(
+        'INSERT INTO numbers(number, sound, description) VALUES (?, ?, ?)',
+        (number, file_to_blob(sound), description))
+
+
+def file_to_blob(path: PathLike | str) -> bytes:
+    """
+    Open a file as a binary blob, and get the contents
+
+    :param path: string or path-like object to file
+    :return: file as binary blob
+    """
+    with open(path, 'rb') as handle:
+        return handle.read()
+
+
+def fetch_audio(cursor: Cursor, number: int) -> bytes:
+    """
+    Get audio from a database entry
+
+    :param cursor: database cursor
+    :param number: entry phone number
+    :return: audio as binary blob
+    """
+    cursor.execute('SELECT sound FROM numbers WHERE number=?', (number,))
+    # there can only be one because number is a unique field
+    return cursor.fetchone()[0]
