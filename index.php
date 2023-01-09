e0244e28919e3595a0629ff72f6d6f90c3de1c68
natejank <njj3397@rit.edu>
2023-01-09 14:36:14 -0500

    handle entry deletion; add field for user notification

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <?php
+    // TODO acquire db location from config file
+    
+    // global notification variable
+    $notify = "";
+
     function phone_entry($number, $filename, $description) {
         $sound_url = "sound.php?number=$number";
         echo <<< HTML
@@ -8,10 +13,46 @@
             <td><a href="$sound_url">$filename</a></td>
             <td>$description</td>
             <td>
-                <form action="" method="POST"><input type="submit" name="$number" value="delete"></form>
+                <form action="" method="POST"><button type="submit" name="delete" value="$number">Delete</button></form>
             </td>
         </tr>
         HTML;
+    }
+
+    // form handling
+    if (isset($_POST['delete'])) {
+        // we're deleting an entry
+        $number = $_POST['delete'];
+        try {
+            // connect to database
+            $db = new SQLite3('phone.db', SQLITE3_OPEN_READWRITE);
+            // create a prepared statement to avoid sql injection
+            $stmt = $db->prepare('DELETE from numbers WHERE number = :num');
+            $stmt->bindValue(':num', $number, SQLITE3_INTEGER);
+            // execute prepared statement
+            $result = $stmt->execute();
+
+            if ($result === false) {
+                // handle errors if execution failed
+                $notify .= '<h2 class="error">'
+                        . "Failed to delete entry $number!  "
+                        . 'Query failed.</h2>';
+            }
+
+            // close connection
+            $db->close();
+
+            // provide user feedback
+            $notify .= "<h3>Deleted entry $number!</h3>";
+        } catch (Exception $e) {
+            // catch exception on DB connection and notify user
+            $notify .= '<h2 class="error">'
+                    . "Failed to delete entry $number!  "
+                    . 'Could not connect to database.  Please contact your System Adminstrator.</h2>';
+        }
+
+    } else if (isset($_POST['create'])) {
+        // we're creating a new entry
     }
 ?>
 
@@ -41,17 +82,25 @@
             </thead>
             <tbody>
                 <?php
-                // TODO get location from configuration
                 try {
+                    // connect to db (read only)
                     $db = new SQLite3('phone.db', SQLITE3_OPEN_READONLY);
 
+                    // get all phone numbers, file names, and descriptions
                     $entries = $db->query('SELECT number, filename, description FROM numbers ORDER BY number ASC');
+                    // loop until we have no more query results
                     while ($row = $entries->fetchArray()) {
+                        // create a table row for each phone number
                         phone_entry($row['number'], $row['filename'], $row['description']);
                     }
+
+                    // close db connection
                     $db->close();
                 } catch (Exception $e) {
-                    echo '<h2 class="error">Could not connect to database!  Please contact your System Adminstrator.</h2>';
+                    // handle error connecting to db; notify user
+                    $notify .= '<h2 class="error">Failed to query numbers!  '
+                            . 'Could not connect to database.  '
+                            . 'Please contact your System Adminstrator.</h2>';
                 }
                 ?>
             </tbody>
@@ -61,7 +110,17 @@
             <input type="text" name="number" placeholder="Phone Number" pattern="^[0-9]+$" minlength=0 maxlength=10>
             <input type="file" name="sound">
             <input type="text" name="description" placeholder="Description">
-            <input type="submit" name="submit" value="submit">
+            <button type="submit" name="create">Submit</button>
         </form>
+    </div>
+    <div id="notify-block">
+        <?php 
+        // block to show user notifications
+        if ($notify !== '') {
+            // show header for context if we have notices to show
+            echo "<h2>Notices</h2>";
+            echo $notify;
+        }
+        ?>
     </div>
 </body>
