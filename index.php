e91c17df43d891380ac50c185b27e589e0e2b4b6
natejank <njj3397@rit.edu>
2023-01-11 23:14:39 -0500

    reimplemented database modification to use p/r/g
    Cleaned up functionality to create a central `phone.php` to be included
    in any phone file
    Created update.php to handle updating/removing entries

--- index.php
+++ index.php
@@ -7,98 +7,8 @@
 <?php
 // TODO acquire db location from config file
 // TODO filter for specific filetypes; get from config file?
-
-/**
- * Global notifications
- */
-class Notifier
-{
-    static $notifications = '';
-
-    public function store($message)
-    {
-        $this::$notifications = $message . $this::$notifications;
-    }
-
-    public function get()
-    {
-        return $this::$notifications;
-    }
-
-    public function clear()
-    {
-        $this::$notifications = '';
-    }
-
-    public function has_notifications()
-    {
-        return $this::$notifications !== '';
-    }
-}
-
-/**
- * Escapes any "special" html characters to combat cross-site scripting.
- *
- * @param String $content content to escape
- * @return String escaped content
- */
-function sanitize_html($content)
-{
-    // sanitize user input to prevent XSS
-    return htmlentities($content, ENT_QUOTES);
-}
-
-/**
- * Converts a string into an "error" tag with styling
- *
- * @param String $message message to display
- * @return String styled message
- */
-function error_msg($message)
-{
-    return '<h2 class="error">'
-        . $message
-        . '</h2>';
-}
-
-/**
- * Converts a string into a "notice" tag with proper styling
- *
- * @param String $message message to display
- * @return String styled message
- */
-function warning_msg($message)
-{
-    return '<h3 class="warning">'
-        . $message
-        . '</h3>';
-}
-
-/**
- * Converts a string into a "notice" tag with proper styling
- *
- * @param String $message message to display
- * @return String styled message
- */
-function notice_msg($message)
-{
-    return '<h3 class="notice">'
-        . $message
-        . '</h3>';
-}
-
-/**
- * Gets file contents as a binary blob
- * @param String $path path to file
- * @return String containing binary file contents
- */
-function get_blob_contents($path)
-{
-    $handle = fopen($path, 'rb');
-    $content = fread($handle, filesize($path));
-    fclose($handle);
-    return $content;
-}
+include "phone.php";
+set_error_handler('error_handler');
 
 /**
  * Create an table row for a phone number
@@ -116,7 +26,7 @@ function phone_entry($number, $filename, $description)
             <td><a href="$sound_url">$filename</a></td>
             <td>$description</td>
             <td>
-                <form action="" method="POST">
+                <form action="update.php" method="POST">
                     <button type="submit" name="delete" value="$number">
                         Delete
                     </button>
@@ -126,176 +36,6 @@ function phone_entry($number, $filename, $description)
         HTML;
 }
 
-/**
- * Check if a POST request was made to delete a database entry.
- */
-function delete_entry()
-{
-    $notify = new Notifier();
-    // strip html tags to prevent XSS when we display to user
-    $number = sanitize_html($_POST['delete']);
-    try {
-        // connect to database
-        $db = new SQLite3('phone.db', SQLITE3_OPEN_READWRITE);
-        // create a prepared statement to avoid sql injection
-        $stmt = $db->prepare('DELETE from numbers WHERE number = :num');
-        $stmt->bindValue(':num', $number, SQLITE3_TEXT);
-        // execute prepared statement
-        $result = $stmt->execute();
-
-        if ($result === false) {
-            // handle errors if execution failed
-            trigger_error("Query failed; did not delete entry $number.", E_USER_WARNING);
-        } else {
-            // provide user feedback
-            $notify->store("<h3>Deleted entry $number!</h3>");
-        }
-
-        // close connection
-        $db->close();
-    } catch (Exception $e) {
-        // catch exception on DB connection and notify user
-        trigger_error($e->getMessage(), E_USER_ERROR);
-    }
-}
-
-/**
- * Check if a POST request was made to create a database entry.
- */
-function create_entry()
-{
-    $notify = new Notifier();
-    // we're creating a new entry
-    // TODO size constraint for files; get from config?
-    // TODO file type constraints; get from config?
-    $file = $_FILES['sound'];
-    // make file size limit more visible
-    if (!isset($file['tmp_name']) | $file['tmp_name'] === '') {
-        trigger_error(
-            'Failed to receive uploaded file.  '
-            . 'Is your file larger than the upload size limit? '
-            . 'Failed to create entry.',
-            E_USER_WARNING
-        );
-        return;
-    }
-    $sound_content_unsafe = get_blob_contents($file['tmp_name']);
-    $number_unsafe = $_POST['number'];
-    $description_unsafe = $_POST['description'];
-    $filename_unsafe = $file['name'];
-
-    // display-safe variables
-    $number = sanitize_html($number_unsafe);
-    $filename = sanitize_html($filename_unsafe);
-    $description = sanitize_html($description_unsafe);
-
-    // validate variables
-    if (!ctype_digit($number_unsafe)) {
-        trigger_error('Failed to create entry. '
-            . "Phone number $number may only contain numeric symbols.",
-            E_USER_NOTICE);
-        return;
-    } elseif (strlen($number_unsafe) > 10 | strlen($number_unsafe) < 1) {
-        trigger_error('Failed to create entry.  '
-            . 'Phone number length must be greater than 1 and less than 10.',
-            E_USER_NOTICE);
-        return;
-    }
-
-    try {
-        // connect to database
-        $db = new SQLite3('phone.db', SQLITE3_OPEN_READWRITE);
-        // create a prepared statement
-        $stmt = $db->prepare('INSERT INTO numbers(number, sound, filename, description) '
-            . 'VALUES (:num, :sound, :filename, :desc)');
-        $stmt->bindValue(':num', $number_unsafe, SQLITE3_TEXT);
-        $stmt->bindValue(':sound', $sound_content_unsafe, SQLITE3_BLOB);
-        $stmt->bindValue(':filename', $filename_unsafe, SQLITE3_TEXT);
-        $stmt->bindValue(':desc', $description_unsafe, SQLITE3_TEXT);
-        //execute prepared statement
-        $result = $stmt->execute();
-
-        if ($result === false) {
-            trigger_error(
-                "Query failed; did not create entry $number.  "
-                . 'Does the number already exist?',
-                E_USER_WARNING);
-        } else {
-            $notify->store("<h3>Created entry $number</h3>");
-        }
-
-        // close connection
-        $db->close();
-    } catch (Exception $e) {
-        trigger_error($e->getMessage(), E_USER_ERROR);
-    }
-}
-
-/**
- * Error handler
- * @param $err_level
- * @param $msg
- */
-function err($errno, $msg, $file, $line)
-{
-    error_log("Processed error $errno in $file on line $line.  $msg");
-    $notify = new Notifier();
-    if (!(error_reporting() & $errno)) {
-        // Error code is not included in error_reporting;
-        // fall back to built-in handler
-        return false;
-    }
-    $msg = htmlspecialchars($msg);
-    switch ($errno) {
-        case E_USER_ERROR:
-            $log_m = "Uncaught fatal error $errno on line $line of $file!  $msg.";
-
-            syslog(LOG_CRIT, $log_m);
-            echo error_msg("$log_m Please contact your System Administrator.");
-            die(1);
-
-        case E_WARNING:
-        case E_USER_WARNING:
-            syslog(LOG_WARNING, $msg);
-            $notify->store(warning_msg($msg));
-            break;
-
-        case E_NOTICE:
-        case E_USER_NOTICE:
-            syslog(LOG_NOTICE, $msg);
-            $notify->store(notice_msg($msg));
-            break;
-        default:
-            // unknown error type
-            $notify->store(error_msg("Unknown error [$errno] $msg"));
-            break;
-    }
-
-    // don't use internal error handler
-    return true;
-}
-
-set_error_handler('err');
-
-// alert for oversized POSTs
-if ($_SERVER['REQUEST_METHOD'] == 'POST'
-    && empty($_POST)
-    && empty($_FILES)
-    && $_SERVER['CONTENT_LENGTH'] > 0) {
-    trigger_error(
-        'Failed to receive uploaded data!  Upload was too large.',
-        E_USER_WARNING
-    );
-}
-
-// form handling
-if (isset($_POST['delete'])) {
-    // we're deleting an entry
-    delete_entry();
-
-} else if (isset($_POST['create'])) {
-    create_entry();
-}
 ?>
 
 <body>
@@ -348,7 +88,7 @@ if ($has_rows === 0) {
 </div>
 <div id="new-entry-block">
     <h3>Add Entry</h3>
-    <form action="" method="POST" enctype="multipart/form-data">
+    <form action="update.php" method="POST" enctype="multipart/form-data">
     <input type="text" name="number" placeholder="Phone Number" pattern="^[0-9]+$" minlength=0 maxlength=10>
     <input type="file" name="sound">
     <input type="text" name="description" placeholder="Description">
---
+++ update.php
@@ -0,0 +1,146 @@
+<?php
+include "phone.php";
+
+set_error_handler('error_handler');
+
+/**
+ * Check if a POST request was made to delete a database entry.
+ */
+function delete_entry()
+{
+    $notify = new Notifier();
+    $result = false;
+    // strip html tags to prevent XSS when we display to user
+    $number = sanitize_html($_POST['delete']);
+    try {
+        // connect to database
+        $db = new SQLite3('phone.db', SQLITE3_OPEN_READWRITE);
+        // create a prepared statement to avoid sql injection
+        $stmt = $db->prepare('DELETE from numbers WHERE number = :num');
+        $stmt->bindValue(':num', $number, SQLITE3_TEXT);
+        // execute prepared statement
+        $result = $stmt->execute();
+
+        if ($result === false) {
+            // handle errors if execution failed
+            trigger_error("Query failed; did not delete entry $number.", E_USER_WARNING);
+        } else {
+            $result = true;
+        }
+
+        // close connection
+        $db->close();
+    } catch (Exception $e) {
+        // catch exception on DB connection and notify user
+        trigger_error($e->getMessage(), E_USER_ERROR);
+    }
+    return $result;
+}
+
+/**
+ * Check if a POST request was made to create a database entry.
+ */
+function create_entry()
+{
+    $notify = new Notifier();
+    $result = false;
+    // we're creating a new entry
+    $file = $_FILES['sound'];
+    // make file size limit more visible
+    if (!isset($file['tmp_name']) | $file['tmp_name'] === '') {
+        trigger_error(
+            'Failed to receive uploaded file.  '
+            . 'Is your file larger than the upload size limit? '
+            . 'Failed to create entry.',
+            E_USER_WARNING
+        );
+        return $result;
+    }
+    $sound_content_unsafe = get_blob_contents($file['tmp_name']);
+    $number_unsafe = $_POST['number'];
+    $description_unsafe = $_POST['description'];
+    $filename_unsafe = $file['name'];
+
+    // display-safe variables
+    $number = sanitize_html($number_unsafe);
+    $filename = sanitize_html($filename_unsafe);
+    $description = sanitize_html($description_unsafe);
+
+    // validate variables
+    if (!ctype_digit($number_unsafe)) {
+        trigger_error('Failed to create entry. '
+            . "Phone number $number may only contain numeric symbols.",
+            E_USER_NOTICE);
+        return $result;
+    } elseif (strlen($number_unsafe) > 10 | strlen($number_unsafe) < 1) {
+        trigger_error('Failed to create entry.  '
+            . 'Phone number length must be greater than 1 and less than 10.',
+            E_USER_NOTICE);
+        return $result;
+    }
+
+    try {
+        // connect to database
+        $db = new SQLite3('phone.db', SQLITE3_OPEN_READWRITE);
+        // create a prepared statement
+        $stmt = $db->prepare('INSERT INTO numbers(number, sound, filename, description) '
+            . 'VALUES (:num, :sound, :filename, :desc)');
+        $stmt->bindValue(':num', $number_unsafe, SQLITE3_TEXT);
+        $stmt->bindValue(':sound', $sound_content_unsafe, SQLITE3_BLOB);
+        $stmt->bindValue(':filename', $filename_unsafe, SQLITE3_TEXT);
+        $stmt->bindValue(':desc', $description_unsafe, SQLITE3_TEXT);
+        //execute prepared statement
+        $result = $stmt->execute();
+
+        if ($result === false) {
+            trigger_error(
+                "Query failed; did not create entry $number.  "
+                . 'Does the number already exist?',
+                E_USER_WARNING);
+        } else {
+            $result = true;
+        }
+
+        // close connection
+        $db->close();
+    } catch (Exception $e) {
+        trigger_error($e->getMessage(), E_USER_ERROR);
+    }
+    return $result;
+}
+
+$result = true;
+$messages = new Notifier();
+
+// alert for oversized POSTs
+if ($_SERVER['REQUEST_METHOD'] == 'POST'
+    && empty($_POST)
+    && empty($_FILES)
+    && $_SERVER['CONTENT_LENGTH'] > 0) {
+    trigger_error(
+        'Failed to receive uploaded data!  Upload was too large.',
+        E_USER_WARNING
+    );
+    $result = false;
+}
+// form handling
+if (isset($_POST['delete'])) {
+    // we're deleting an entry
+    $result = delete_entry();
+} else if (isset($_POST['create'])) {
+    // we're creating an entry
+    $result = create_entry();
+}
+
+// if we succeeded, redirect to destination
+if ($result !== false) {
+    $protocol = is_using_https() ? "https" : "http";
+    $host = $_SERVER['HTTP_HOST'];
+
+    header("HTTP/1.1 303 See Other");
+    header("Location: $protocol://$host/index.php");
+}
+
+// otherwise, display error messages
+echo $messages->get();
+?>
