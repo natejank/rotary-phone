8899b79ba4af196d9d595f1c5dfc4ec755dd50f7
Nathan Jankowski <45954802+natejank@users.noreply.github.com>
2023-02-02 23:24:43 -0500

    add mozilla public license v2 (#3)

--- res/index.php
+++ res/index.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * Copyright (C) 2023 Nathan Jankowski
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 /**
  * Main dashboard page; list entries and allow adding/removing more
  *
--- res/sound.php
+++ res/sound.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * Copyright (C) 2023 Nathan Jankowski
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 /**
  * Serve sound file from database by phone number.
  * Opens database, reads file name and content, then serves it as a document.
